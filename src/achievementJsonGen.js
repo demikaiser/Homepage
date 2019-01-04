7a97d642d4f8d74d7417c3df8ee75c17bfcb8fad
Jake Choi <demikaiser13@gmail.com>
2019-01-03 19:52:18 -0800

    Updates.

--- src/achievementJsonGen.js
+++ src/achievementJsonGen.js
@@ -27,8 +27,36 @@ var achievement = {
   "vlogs": []
 };
 
+// Search information.json files.
+var getAllEntriesFromOneFolder = (folder) => {
 
+  let dirs = fs.readdirSync(currentDir + '/achievement/' + folder);
 
+  for (let i = 0; i < dirs.length; i++) {
+    let data = fs.readFileSync(currentDir + '/achievement/' + folder + '/' + dirs[i] + '/information.json', 'utf8');
+    let jsonFromData = JSON.parse(data);
+
+    let jsonToPush = {
+      "pathHtml": '../achievement/' + folder + '/' + dirs[i] + '/index.html',
+      "pathThumbnail": '../achievement/' + folder + '/' + dirs[i] + '/thumbnail.jpg',
+      "title": jsonFromData.title,
+      "subtitle": jsonFromData.subtitle,
+      "description": jsonFromData.description
+    };
+
+    achievement[folder].push(jsonToPush);
+  }
+
+}
+
+getAllEntriesFromOneFolder('blogs');
+getAllEntriesFromOneFolder('portfolios');
+getAllEntriesFromOneFolder('vlogs');
+
+// Reverse the elements to get the most latest articles on the top.
+achievement.blogs = achievement.blogs.reverse();
+achievement.portfolios = achievement.portfolios.reverse();
+achievement.vlogs = achievement.vlogs.reverse();
 
 // ######## ######## ######## ######## ######## ######## ######## ########
 // #                          Finalize & Store                           #
